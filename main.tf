0dd317cd6d0c2d644c544411d558c41e14313210
William Hearn <sylus1984@gmail.com>
2020-11-23 12:32:03 -0500

    feat(starboard): Switch to Starboard Operator + use Helm chart for install

--- main.tf
+++ main.tf
@@ -41,6 +41,6 @@ resource "null_resource" "dependency_setter" {
   # https://github.com/hashicorp/terraform/issues/1178#issuecomment-449158607
   # List resource(s) that will be constructed last within the module.
   depends_on = [
-    helm_resource.starboard_operator.id,
+    helm_resource.starboard_operator,
   ]
 }
